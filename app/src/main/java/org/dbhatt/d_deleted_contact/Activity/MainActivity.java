daa719c3f4f545372442663cecc9e5f72c1c3a80
devdatt bhatt <devsbhattleb@gmail.com>
2016-10-03 04:34:13 +0530

    starting info dialog box

--- app/src/main/java/org/dbhatt/d_deleted_contact/Activity/MainActivity.java
+++ app/src/main/java/org/dbhatt/d_deleted_contact/Activity/MainActivity.java
@@ -1,16 +1,21 @@
 package org.dbhatt.d_deleted_contact.Activity;
 
 import android.Manifest;
+import android.content.ContentProviderOperation;
 import android.content.ContentResolver;
 import android.content.Intent;
+import android.content.OperationApplicationException;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
+import android.os.RemoteException;
 import android.provider.ContactsContract;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
@@ -19,10 +24,16 @@ import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.appinvite.AppInvite;
@@ -31,8 +42,6 @@ import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 
 import org.dbhatt.d_deleted_contact.Data.Contact;
-import org.dbhatt.d_deleted_contact.Fragment.All_contact;
-import org.dbhatt.d_deleted_contact.Fragment.Deleted_contact;
 import org.dbhatt.d_deleted_contact.R;
 
 import java.io.File;
@@ -40,7 +49,7 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final int DO_NOT_FINISH_REQUEST_CODE = 143, APP_INVITE = 9211;
+    private static final int DO_NOT_FINISH_REQUEST_CODE = 143, APP_INVITE = 9211, SHARE_APP = 142;
     private static boolean refreshing = false;
     private static ContentResolver contentResolver;
     private static int READ_CONTACT_PERMISSION_REQUEST = 0000;
@@ -48,12 +57,16 @@ public class MainActivity extends AppCompatActivity {
     private SectionsPagerAdapter mSectionsPagerAdapter;
     private ViewPager mViewPager;
     private ArrayList<Contact> all_contact, deleted_contact;
+    All_contact fragment_all_contact;
+    Deleted_contact fragment_deleted_contact;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        fragment_all_contact = new All_contact();
+        fragment_deleted_contact = new Deleted_contact();
 
         finish_activity = false;
         new Thread(new Runnable() {
@@ -120,8 +133,7 @@ public class MainActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.action_restore_all:
-                if (deleted_contact.isEmpty())
-                    Toast.makeText(this, R.string.no_data_found, Toast.LENGTH_SHORT).show();
+                new Restore_All_contact().executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
                 break;
             case R.id.action_app_invite:
                 finish_activity = false;
@@ -133,7 +145,6 @@ public class MainActivity extends AppCompatActivity {
 
                             }
                         }).build();
-
                 Intent app_invite_intent = new AppInviteInvitation.IntentBuilder(getString(R.string.invite_title))
                         .setMessage(getString(R.string.invite_message))
                         .setEmailSubject(getString(R.string.invite_email_subject))
@@ -143,7 +154,12 @@ public class MainActivity extends AppCompatActivity {
                 break;
 
             case R.id.action_share:
-
+                finish_activity = false;
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_SEND);
+                intent.setType("application/zip");
+                intent.putExtra(Intent.EXTRA_STREAM, Uri.fromFile(new File(getApplicationInfo().sourceDir)));
+                startActivityForResult(Intent.createChooser(intent, getString(R.string.share)), APP_INVITE);
                 break;
 
             case R.id.action_contact_us:
@@ -162,6 +178,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onPause() {
         super.onPause();
+        Log.e("on pause", String.valueOf(finish_activity));
         if (finish_activity)
             finish();
     }
@@ -175,6 +192,13 @@ public class MainActivity extends AppCompatActivity {
                 break;
             case APP_INVITE:
                 finish_activity = true;
+                if (resultCode == RESULT_OK)
+                    Toast.makeText(this, R.string.thank_you, Toast.LENGTH_SHORT).show();
+                break;
+            case SHARE_APP:
+                finish_activity = true;
+                if (resultCode == RESULT_OK)
+                    Toast.makeText(this, R.string.thank_you, Toast.LENGTH_SHORT).show();
                 break;
         }
     }
@@ -196,11 +220,11 @@ public class MainActivity extends AppCompatActivity {
         public Fragment getItem(int position) {
             switch (position) {
                 case 0:
-                    return new All_contact();
+                    return fragment_all_contact;
                 case 1:
-                    return new Deleted_contact();
+                    return fragment_deleted_contact;
                 default:
-                    return new All_contact();
+                    return fragment_all_contact;
             }
         }
 
@@ -250,9 +274,6 @@ public class MainActivity extends AppCompatActivity {
                 }
                 if (cursor != null && !cursor.isClosed())
                     cursor.close();
-                Log.e("all data", String.valueOf(all_contact.size()));
-                Log.e("deleted data", String.valueOf(deleted_contact.size()));
-
             } catch (Exception e) {
                 e.printStackTrace();
             }
@@ -264,7 +285,67 @@ public class MainActivity extends AppCompatActivity {
             super.onPostExecute(aVoid);
             refreshing = false;
         }
+    }
 
+    class Restore_All_contact extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            if (deleted_contact.isEmpty())
+                Toast.makeText(getApplicationContext(), R.string.no_data_found, Toast.LENGTH_SHORT).show();
+            else {
+                for (int i = 0; i < deleted_contact.size(); i++) {
+                    ArrayList<ContentProviderOperation> ops = new ArrayList();
+                    ops.add(ContentProviderOperation.newUpdate(ContactsContract.RawContacts.CONTENT_URI.buildUpon().appendQueryParameter("caller_is_syncadapter", "true").build())
+                            .withSelection(ContactsContract.RawContacts._ID + "=?", new String[]{String.valueOf(deleted_contact.get(i).getId())})
+                            .withValue(ContactsContract.RawContacts.DELETED, 0)
+                            .withYieldAllowed(true)
+                            .build());
+                    try {
+                        contentResolver.applyBatch(ContactsContract.AUTHORITY, ops);
+                    } catch (RemoteException e) {
+                        e.printStackTrace();
+                    } catch (OperationApplicationException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+            refreshing = true;
+            deleted_contact.clear();
+            all_contact.clear();
+            try {
+                Cursor cursor = contentResolver.query(ContactsContract.RawContacts.CONTENT_URI, null, "DISPLAY_NAME IS NOT NULL", null, "display_name COLLATE LOCALIZED ASC");
+                if (cursor != null && cursor.getCount() > 0 && cursor.moveToFirst()) {
+                    while (cursor.moveToNext()) {
+                        if (cursor.getInt(cursor.getColumnIndex(ContactsContract.RawContacts.DELETED)) == 1) {
+                            deleted_contact.add(new Contact(cursor.getInt(cursor.getColumnIndex(ContactsContract.RawContacts._ID)),
+                                    cursor.getInt(cursor.getColumnIndex(ContactsContract.RawContacts.CONTACT_ID)),
+                                    cursor.getString(cursor.getColumnIndex(ContactsContract.RawContacts.DISPLAY_NAME_PRIMARY)),
+                                    cursor.getString(cursor.getColumnIndex(ContactsContract.RawContacts.ACCOUNT_TYPE)),
+                                    "1"));
+                        } else {
+                            all_contact.add(new Contact(cursor.getInt(cursor.getColumnIndex(ContactsContract.RawContacts._ID)),
+                                    cursor.getInt(cursor.getColumnIndex(ContactsContract.RawContacts.CONTACT_ID)),
+                                    cursor.getString(cursor.getColumnIndex(ContactsContract.RawContacts.DISPLAY_NAME_PRIMARY)),
+                                    cursor.getString(cursor.getColumnIndex(ContactsContract.RawContacts.ACCOUNT_TYPE)),
+                                    "0"));
+                        }
+                    }
+                }
+                if (cursor != null && !cursor.isClosed())
+                    cursor.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+            fragment_all_contact.update();
+            fragment_deleted_contact.update();
+        }
     }
 
     public ArrayList<Contact> get_all_contact() {
@@ -277,5 +358,99 @@ public class MainActivity extends AppCompatActivity {
 
     public void setFinish_activity(boolean finish_activity) {
         this.finish_activity = finish_activity;
+    }
+
+    public static class All_contact extends Fragment {
+
+        View view;
+        ArrayList<Contact> all_contact;
+        RecyclerView recyclerView;
+        TextView data_not_found;
+
+        @Nullable
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+
+            all_contact = ((MainActivity) getActivity()).get_all_contact();
+            view = inflater.inflate(R.layout.recyclerview, container, false);
+            recyclerView = (RecyclerView) view.findViewById(R.id.adapter_recycler_view);
+            data_not_found = (TextView) view.findViewById(R.id.no_data_found);
+
+            if (all_contact.size() > 0) {
+                data_not_found.setVisibility(View.GONE);
+                recyclerView.setVisibility(View.VISIBLE);
+                recyclerView.setHasFixedSize(true);
+                recyclerView.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL, false));
+                recyclerView.setAdapter(new org.dbhatt.d_deleted_contact.Data.All_contact(all_contact, getContext(), ((MainActivity) getActivity())));
+                recyclerView.setNestedScrollingEnabled(false);
+            } else {
+                data_not_found.setVisibility(View.VISIBLE);
+                recyclerView.setVisibility(View.GONE);
+            }
+            return view;
+        }
+
+        public void update() {
+            all_contact = ((MainActivity) getActivity()).get_all_contact();
+            if (all_contact.size() > 0) {
+                data_not_found.setVisibility(View.GONE);
+                recyclerView.setVisibility(View.VISIBLE);
+                recyclerView.setHasFixedSize(true);
+                recyclerView.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL, false));
+                recyclerView.setAdapter(new org.dbhatt.d_deleted_contact.Data.All_contact(all_contact, getContext(), ((MainActivity) getActivity())));
+                recyclerView.setNestedScrollingEnabled(false);
+            } else {
+                data_not_found.setVisibility(View.VISIBLE);
+                recyclerView.setVisibility(View.GONE);
+            }
+        }
+    }
+
+    public static class Deleted_contact extends Fragment {
+
+        RecyclerView recyclerView;
+        View view;
+        ArrayList<Contact> deleted_contact;
+        TextView data_not_found;
+
+        @Nullable
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+            view = inflater.inflate(R.layout.recyclerview, container, false);
+
+            recyclerView = (RecyclerView) view.findViewById(R.id.adapter_recycler_view);
+            data_not_found = (TextView) view.findViewById(R.id.no_data_found);
+            deleted_contact = ((MainActivity) getActivity()).get_deleted_contact();
+
+            if (deleted_contact.size() > 0) {
+                data_not_found.setVisibility(View.GONE);
+                recyclerView.setVisibility(View.VISIBLE);
+                recyclerView.setHasFixedSize(true);
+                recyclerView.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL, false));
+                recyclerView.setAdapter(new org.dbhatt.d_deleted_contact.Data.Deleted_contact(deleted_contact, getContext()));
+                recyclerView.setNestedScrollingEnabled(false);
+            } else {
+                data_not_found.setVisibility(View.VISIBLE);
+                recyclerView.setVisibility(View.GONE);
+            }
+            return view;
+        }
+
+        public void update() {
+            deleted_contact = ((MainActivity) getActivity()).get_deleted_contact();
+            if (deleted_contact.size() > 0) {
+                data_not_found.setVisibility(View.GONE);
+                recyclerView.setVisibility(View.VISIBLE);
+                recyclerView.setHasFixedSize(true);
+                recyclerView.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL, false));
+                recyclerView.setAdapter(new org.dbhatt.d_deleted_contact.Data.All_contact(deleted_contact, getContext(), ((MainActivity) getActivity())));
+                recyclerView.setNestedScrollingEnabled(false);
+            } else {
+                data_not_found.setVisibility(View.VISIBLE);
+                recyclerView.setVisibility(View.GONE);
+            }
+        }
     }
 }
--- app/src/main/java/org/dbhatt/d_deleted_contact/Data/All_contact.java
+++ app/src/main/java/org/dbhatt/d_deleted_contact/Data/All_contact.java
@@ -44,7 +44,7 @@ import java.util.Random;
 public class All_contact extends RecyclerView.Adapter<All_contact.Contact> {
 
     private static final int DO_NOT_FINISH_REQUEST_CODE = 143;
-    private static MainActivity mainActivity;
+    private MainActivity mainActivity;
     private Context context;
     private boolean rtl = false;
     private Random rnd;
@@ -86,13 +86,13 @@ public class All_contact extends RecyclerView.Adapter<All_contact.Contact> {
         return all_contact.size();
     }
 
-    public class Contact extends RecyclerView.ViewHolder implements View.OnClickListener {
+    class Contact extends RecyclerView.ViewHolder implements View.OnClickListener {
 
         CardView contact_raw;
         TextView contact_name, account_type;
         ImageView contact_photo, contact_info;
 
-        public Contact(View itemView) {
+        Contact(View itemView) {
             super(itemView);
 
             contact_raw = (CardView) itemView.findViewById(R.id.contact_raw);
@@ -110,55 +110,77 @@ public class All_contact extends RecyclerView.Adapter<All_contact.Contact> {
         public void onClick(View v) {
             switch (v.getId()) {
                 case R.id.contact_raw:
+
                     break;
                 case R.id.contact_photo:
                     final Cursor cursor = resolver.query(ContactsContract.Data.CONTENT_URI, new String[]{ContactsContract.Data.DATA15},
                             ContactsContract.Data.RAW_CONTACT_ID + "=? AND " + ContactsContract.Data.MIMETYPE + " =?",
                             new String[]{String.valueOf(all_contact.get(getAdapterPosition()).getId()), android.provider.ContactsContract.CommonDataKinds.Photo.CONTENT_ITEM_TYPE}
                             , null);
-                    if (cursor.moveToFirst()) {
-                        final byte[] photo = cursor.getBlob(0);
-                        cursor.close();
-                        if (photo != null) {
-                            final InputStream inputStream = new ByteArrayInputStream(photo);
-                            final Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
-                            View view = ((LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE)).inflate(R.layout.alert_dialog_photo, null);
-                            ((ImageView) view.findViewById(R.id.contact_photo)).setImageBitmap(bitmap);
-                            AlertDialog.Builder builder = new AlertDialog.Builder(context)
-                                    .setNegativeButton(R.string.dismiss, null)
-                                    .setView(view)
-                                    .setPositiveButton(R.string.share, new DialogInterface.OnClickListener() {
-                                        @Override
-                                        public void onClick(DialogInterface dialog, int which) {
-                                            mainActivity.setFinish_activity(false);
-                                            File tmp_file = null;
-                                            try {
-                                                tmp_file = new File(Environment.getExternalStorageDirectory(), "tmp.png");
-                                                FileOutputStream outputStream = new FileOutputStream(tmp_file);
-                                                bitmap.compress(Bitmap.CompressFormat.PNG, 100, outputStream);
-                                                outputStream.flush();
-                                                outputStream.close();
-                                            } catch (FileNotFoundException e) {
-                                                e.printStackTrace();
-                                            } catch (IOException e) {
-                                                e.printStackTrace();
+                    if (cursor != null)
+                        if (cursor.moveToFirst()) {
+                            final byte[] photo = cursor.getBlob(0);
+                            cursor.close();
+                            if (photo != null) {
+                                final InputStream inputStream = new ByteArrayInputStream(photo);
+                                final Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
+                                View view = ((LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE)).inflate(R.layout.alert_dialog_photo, null);
+                                ((ImageView) view.findViewById(R.id.contact_photo)).setImageBitmap(bitmap);
+                                AlertDialog.Builder builder = new AlertDialog.Builder(context)
+                                        .setNegativeButton(R.string.dismiss, null)
+                                        .setView(view)
+                                        .setPositiveButton(R.string.share, new DialogInterface.OnClickListener() {
+                                            @Override
+                                            public void onClick(DialogInterface dialog, int which) {
+                                                mainActivity.setFinish_activity(false);
+                                                File tmp_file = null;
+                                                try {
+                                                    tmp_file = new File(Environment.getExternalStorageDirectory(), "tmp.png");
+                                                    FileOutputStream outputStream = new FileOutputStream(tmp_file);
+                                                    bitmap.compress(Bitmap.CompressFormat.PNG, 100, outputStream);
+                                                    outputStream.flush();
+                                                    outputStream.close();
+                                                } catch (FileNotFoundException e) {
+                                                    e.printStackTrace();
+                                                } catch (IOException e) {
+                                                    e.printStackTrace();
+                                                }
+                                                Intent sendIntent = new Intent();
+                                                sendIntent.setAction(Intent.ACTION_SEND);
+                                                sendIntent.putExtra(Intent.EXTRA_STREAM, Uri.fromFile(tmp_file));
+                                                sendIntent.setType("image/png");
+                                                mainActivity.startActivityForResult(Intent.createChooser(sendIntent, context.getText(R.string.share)), DO_NOT_FINISH_REQUEST_CODE);
                                             }
-                                            Intent sendIntent = new Intent();
-                                            sendIntent.setAction(Intent.ACTION_SEND);
-                                            sendIntent.putExtra(Intent.EXTRA_STREAM, Uri.fromFile(tmp_file));
-                                            sendIntent.setType("image/png");
-                                            mainActivity.startActivityForResult(Intent.createChooser(sendIntent, context.getText(R.string.share)), DO_NOT_FINISH_REQUEST_CODE);
-                                        }
-                                    });
-                            builder.create().show();
+                                        });
+                                builder.create().show();
+                            }
                         }
-                    }
                     break;
                 case R.id.contact_info:
-                    View view = ((LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE)).inflate(R.layout.alert_dialog_photo, null);
                     AlertDialog.Builder builder = new AlertDialog.Builder(context);
-                    builder.setTitle("");
-                    builder.setView(view);
+                    builder.setTitle(all_contact.get(getAdapterPosition()).getName());
+                    Cursor info_cursor = resolver.query(
+                            ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+                            null,
+                            ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = ?",
+                            new String[]{String.valueOf(all_contact.get(getAdapterPosition()).getId())},
+                            null);
+                    if (info_cursor != null) {
+                        View view = ((LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE)).inflate(R.layout.contact_about, null);
+                        int[] d = new int[]{R.id.number0, R.id.number1, R.id.number2, R.id.number3, R.id.number4, R.id.number5, R.id.number6};
+                        for (int i = 0; info_cursor.moveToNext() && i < 6; i++) {
+                            view.findViewById(d[i]).setVisibility(View.VISIBLE);
+                            ((TextView) view.findViewById(d[i])).setText(info_cursor.getString(info_cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER)));
+                        }
+                        if ((view.findViewById(d[0])).getVisibility() == View.VISIBLE)
+                            builder.setView(view);
+                        else builder.setMessage(R.string.no_data_found);
+
+                    } else
+                        builder.setMessage(R.string.no_data_found);
+
+                    builder.setNegativeButton(R.string.dismiss, null);
+                    builder.create().show();
                     break;
             }
         }
